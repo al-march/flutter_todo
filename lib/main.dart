c37c0cba5324ce83ac6b53f2c4df81bf0a556dbd
Alexander <valverin95@gmail.com>
2023-03-12 00:35:27 +0300

    add state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/pages/start_page.dart';
-import 'package:todo_app/pages/todo_page.dart';
+
+import 'models/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Todo',
-      theme: ThemeData(
-        primarySwatch: Colors.teal, 
-        scaffoldBackgroundColor: const Color(0xFFEDEDED)
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Flutter Todo',
+        theme: ThemeData(
+          useMaterial3: true,
+          primaryColor: Colors.teal,
+          scaffoldBackgroundColor: const Color(0xFFEDEDED),
+        ),
+        home: const StartPage(),
       ),
-      home: const StartPage(),
+    );
+  }
+}
+
+class AppState extends ChangeNotifier {
+  List<Todo> todos = [];
+
+  addTodo(String name) {
+    var todo = createTodo(name);
+    todos.insert(0, todo);
+    notifyListeners();
+    return todo;
+  }
+
+  deleteTodo(String id) {
+    todos.removeWhere((todo) => todo.id == id);
+    notifyListeners();
+  }
+
+  createTodo(String name) {
+    var id = DateTime.now().toString();
+    return Todo(
+      name: name,
+      id: id,
     );
   }
 }
